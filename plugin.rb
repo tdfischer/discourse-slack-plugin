396ba1436c0a007737a332493f59f15c1a2fc0fe
Torrie Fischer <tdfischer@hackerbots.net>
2016-08-08 21:06:52 -0700

    switch ot use SiteSetting for configuration

--- plugin.rb
+++ plugin.rb
@@ -7,23 +7,26 @@ require 'json'
 require 'net/http'
 require 'pp'
 
-SLACK_HOOK_URL ||= ENV['SLACK_HOOK_URL'].freeze
-
 DiscourseEvent.on(:post_created) do |post|
-  attachment_data = {
-    :fallback => "New in #{post.topic.category.name}: #{post.topic.title}",
-    :color => "#" + post.topic.category.color,
-    :author => {
-      :author_name => post.user.readable_name,
-      :author_icon => post.user.small_avatar_url
-    },
-    :title => "New post in #{post.topic.title}",
-    :title_link => post.full_url,
-    :text => post.raw,
-    :footer => post.topic.category.name,
-  }
-  data = {:username => "Discourse", :icon_emoji => ":oob:", :attachments => [attachment_data]}
-  slack_uri = URI(SLACK_HOOK_URL)
-  Net::HTTP::post_form(slack_uri, 'payload' => ActiveSupport::JSON.encode(data))
-  Rails.logger.info("Sending new post to slack.")
+  if SiteSetting.slack_enabled
+    if post.post_type == Post.types[:regular]
+      attachment_data = {
+        :fallback => "#{post.topic.category.name}: #{post.topic.title}",
+        :color => "#" + post.topic.category.color,
+        :author => {
+          :author_name => post.user.readable_name,
+          :author_icon => post.user.small_avatar_url
+        },
+        :title => "New post in #{post.topic.title}",
+        :title_link => post.full_url,
+        :text => post.raw,
+        :footer => post.topic.category.name,
+        :mrkdwn_in => ['text']
+      }
+      data = {:username => "Discourse", :icon_emoji => ":oob:", :attachments => [attachment_data]}
+      slack_uri = URI(SiteSetting.slack_webhook_url)
+      Net::HTTP::post_form(slack_uri, 'payload' => ActiveSupport::JSON.encode(data))
+      Rails.logger.info("Sending new post to slack.")
+    end
+  end
 end
